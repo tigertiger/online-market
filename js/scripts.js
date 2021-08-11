36cb6d3c19ffcf9bce7f664b03b82faa358fffed
Melissa <melissa.scmi@gmail.com>
2021-08-11 16:17:46 -0700

    add radio buttons and js functionality for them
    Co-authored-by: Tajo Fisher <tajofisher4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
     const customerNameInput = $("input#customerName").val();
     const customerAddressInput = $("input#customerAddress").val();
     const customerItemInput = $("input#customerItem").val();
+    const quantityInput = $("#quantity").val();
+    const colorInput = $("input:radio[name=color]:checked").val();
     
     $(".customerName").append(customerNameInput);
     $(".customerAddress").append(customerAddressInput);
     $(".customerItem").append(customerItemInput);
+    $(".quantity").append(quantityInput);
+    $(".color").append(colorInput);
 
     $("#receipt").show();
   });
